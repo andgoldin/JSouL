d2f0e36eecf84d4ea26cc927080ac6c75084f41a
Andrew Goldin <andgoldin@gmail.com>
2013-07-03 02:30:12 -0400

    Update JSouLTester.java

--- src/JSouLTester.java
+++ src/JSouLTester.java
@@ -1,14 +1,30 @@
 import jsoul.midi.*;
 
+// Some JSouL example code.
 public class JSouLTester {
+	
 	public static void main(String[] args) throws Exception {
+		
+		// create a new Track object
 		Track t = new Track();
-		for (int i = 0; i < 1500; i++) {
-			t.add(new Note((int) (Math.random() * 30 + 60),Dynamics.FORTE, Note.SIXTYFOURTH));
+		
+		// add a sequence of 150 random notes between F1 and F6 to the Track
+		for (int i = 0; i < 150; i++) {
+			t.add(new Note((int) (Math.random() * 30 + 60), Dynamics.FORTE, Note.SIXTYFOURTH));
 		}
+		
+		// set instrument to honky tony piano
 		t.setInstrument(Instrument.HONKY_TONK_PIANO);
+		
+		// create a new Sequence object holding the sole Track
 		Sequence s = new Sequence(t);
-		s.setTempoInBPM(700);
+		
+		// set the sequence tempo
+		s.setTempoInBPM(150);
+		
+		// play the sequence
 		Player.play(s);
+		
 	}
+	
 }
